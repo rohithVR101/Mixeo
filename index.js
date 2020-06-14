c4ed00858040cc917beb580be63890bb81e1a51c
rohithVR101 <rohithriaz11@gmail.com>
2020-06-14 15:41:26 +0530

    prepare for initial deploy

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var cors = require('cors');
 const app = express();
 const routes = require('./routes');
 app.set('view engine', 'ejs');
@@ -8,15 +7,11 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-var corsOptions = {
-    origin: "http://localhost:3000"
-};
-app.use(cors(corsOptions));
 
 app.use(express.static("public"));
 
 app.use('/', routes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Running");
 });
